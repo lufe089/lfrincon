4ac9da4798469cc6b22dc3300fc27f7526af2294
lufe089 <lufe089@gmail.com>
2020-02-16 23:12:33 -0500

    NEW: Ejercicios de ciclos

---
+++ material/IntroProg/Ejercicios/3. Ciclos/SeguimientoBasico.py
@@ -0,0 +1,41 @@
+def seguimientoCiclosV1(max):
+    cont=1
+    while cont<=max:
+        print(cont)
+        cont=cont+1
+
+def seguimientoCiclos():
+    cont=1
+    while cont<=20:
+        print(cont)
+        cont=cont+1
+
+def seguimientoCiclos2(max):
+    cont=1
+    while cont<=max:
+        print("Hola")
+
+def seguimientoCiclos2V1(max):
+    cont=1
+    while cont<=max:
+        print("Hola")
+        cont=cont+1
+
+def seguimientoCiclos2V2(max):
+    cont=1
+    print("Hola seg2V2")
+    while cont<=max:
+        cont=cont+1
+
+def seguimientoCiclos2V2(max):
+    cont=1
+    while cont<=max:
+        if cont>5:
+             print("Hola seg2V2")    
+        cont=cont+1
+
+
+seguimientoCiclos2V1(10)
+seguimientoCiclos2V2(10)
+seguimientoCiclosV1(10)
+seguimientoCiclos()
