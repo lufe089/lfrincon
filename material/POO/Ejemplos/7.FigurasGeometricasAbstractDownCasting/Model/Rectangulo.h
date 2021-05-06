634273bac76a4819dc5509cc11182abf865d2480
Luisa Rincon <lufe089@gmail.com>
2021-05-06 16:50:24 -0500

    MOD: Actualizacion notas

---
+++ material/POO/Ejemplos/7.FigurasGeometricasAbstractDownCasting/Model/Rectangulo.h
@@ -0,0 +1,34 @@
+//#ifndef RECTANGULO_H
+// Guardas
+//#define RECTANGULO_H
+
+//CLASE RECTANGULO
+#include <iostream>
+#include <cstdlib>
+
+#include "FiguraGeometrica.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Rectangulo : public FiguraGeometrica
+{
+private:
+    float ancho, largo;
+
+public:
+    Rectangulo(); //no olvidar
+    Rectangulo(float, float);
+    void mostrarFigura() const;
+    virtual void calcularArea();
+    virtual void calcularPerimetro();
+    virtual void mostrarFigura(); // sobreescritura
+    void soloRectangulo();
+    float getLargo();
+    float getAncho();
+    void setLargo(float);
+    void setAncho(float);
+};
+
+//#endif /* !RECTANGULO_H */
