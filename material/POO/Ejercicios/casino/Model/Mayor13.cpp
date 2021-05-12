1a46209fcbfd03d1031a7839cdc70bc4336c3df6
Luisa Rincon <lufe089@gmail.com>
2021-05-12 11:01:06 -0500

    NEW: esqueleto proyecto casino

---
+++ material/POO/Ejercicios/casino/Model/Mayor13.cpp
@@ -0,0 +1,23 @@
+//
+// Created by lufe0 on 7/05/2021.
+//
+
+#include "Mayor13.h"
+
+float Mayor13::jugar(float gonzosApostar) {
+    srand(time(NULL));
+
+    cout << "Por implementar, mire el ejemplo en DosColores y la logica a usar en el documento \n";
+
+    return 0;
+}
+
+
+float Mayor13::calcularResultado(float gonzosApostar) {
+    cout << "Por implementar, mire el ejemplo en DosColores y la logica a usar en el documento \n";
+
+}
+
+Mayor13::~Mayor13() {
+
+}
